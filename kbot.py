27d5c189bb3f2064e4da685b4fb6599259a29931
Keegan Nave <Keegan.nave@gmail.com>
2017-12-17 19:15:47 -0800

    adds an info function and a commands function

--- kbot.py
+++ kbot.py
@@ -7,18 +7,34 @@ import asyncio
 
 bot = commands.Bot(command_prefix="#")
 
+#Displays when ran
 @bot.event
 async def on_ready():
-	print ("Readyyyyyy")
+	print ("Ready")
 	print ("I am running on " + bot.user.name)
 	print ("With the ID: " + bot.user.id)
 
+#"Pings" the user
 @bot.command(pass_context=True)
 async def ping(ctx):
-	await bot.say(":ping_pong: ping! xSSS")
+	await bot.say(":ping_pong: ping!")
 	print("user has pinged")
 
+#Prints out user info
 @bot.command(pass_context=True)
+async def  info(ctx, user: discord.Member):
+	await bot.say("The username is: {}".format(user.name))
+	await bot.say("The users ID is: {}".format(user.id))
+	await bot.say("The users status is: {}".format(user.status))
+	await bot.say("The users highest role is: {}".format(user.top_role))
+	await bot.say("The user joined at: {}".format(user.joined_at))
+
+#Displays command list
+@bot.command(pass_context=True)
+async def commands(ctx):
+	await bot.say("To ping the bot: #ping")
+	await bot.say("To get info on a user: #info @user")
+	await bot.say("More commands coming soon!")
 
 bot.run("MzkxMzA5MjE1NjY0MTc3MTUy.DRWyrA.ExifRHOC354vDucFiSFWs7ewVmc")
 
